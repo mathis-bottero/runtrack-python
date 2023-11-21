3832f21de0a2976bf0d2b82402c6bf92ff74d5a9
mathis-bottero <mathis.bottero@laplateforme.io>
2023-11-21 15:24:19 +0100

    Travail Fait

--- jour02/job07/main.py
+++ jour02/job07/main.py
@@ -4,18 +4,9 @@ chaine = "abcdefghijklmnopqrstuvwxyz" * 10
 nombre_total_caracteres = 50
 
 
-compteur_caracteres = 0
+chaine = "abcdefghijklmnopqrstuvwxyz"
+n = 10
 
-
-for i in range(1, 12, 2):
-    espace = " " * ((11 - i) // 2)
-    partie_chaine = chaine[compteur_caracteres:compteur_caracteres + i]
-    ligne = espace + partie_chaine + espace
+for i in range(1, n + 1):
+    ligne = chaine[:i]
     print(ligne)
-    
-
-    compteur_caracteres += i
-
-
-    if compteur_caracteres >= nombre_total_caracteres:
-        break
